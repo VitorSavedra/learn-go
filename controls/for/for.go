be62782da52abe82eabe278d73e0e12d37c5be8d
Vitor Savedra <vitor-savedra@outlook.com>
2020-05-20 18:59:24 -0300

    :pencil: Learning about for

---
+++ controls/for/for.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+
+	for i := 0; i < 20; i += 2 {
+		fmt.Printf("%d ", i)
+	}
+
+	for i := 0; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Print("Even ")
+		} else {
+			fmt.Print("Odd ")
+		}
+	}
+
+	for {
+		fmt.Println("Forever...")
+		time.Sleep(time.Second * 5)
+	}
+}
