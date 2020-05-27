f50e7917f174b890e4e605b5e5fecf9315e66f6f
Vitor Savedra <vitor-savedra@outlook.com>
2020-05-27 14:48:47 -0300

    :pencil: Learning about packages (install, visibility, reuse and more)

---
+++ packages/reuse/reuse.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cod3rcursos/area"
+)
+
+func main() {
+	fmt.Println(area.Circle(6.0))
+	fmt.Println(area.Rectangle(5.0, 2.0))
+	// fmt.Println(area._EquilateralTriangle(5.0, 2.0))
+}
